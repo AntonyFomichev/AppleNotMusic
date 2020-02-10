c1b29893f1f639876f5f87335caf05c2e09d1c8e
deviantkun <deviant_kun@protonmail.com>
2020-02-10 10:10:02 +0300

    feat: 🎸 Add scale animation on track details image

--- AppleNotMusic/Search/TrackDetailView/TrackDetailView.swift
+++ AppleNotMusic/Search/TrackDetailView/TrackDetailView.swift
@@ -29,6 +29,10 @@ class TrackDetailView:UIView {
   override func awakeFromNib() {
     super.awakeFromNib()
     
+    let scale: CGFloat = 0.8
+    trackImageView.transform = CGAffineTransform(scaleX: scale, y: scale)
+    
+    trackImageView.layer.cornerRadius = 5
     trackImageView.backgroundColor = .gray
   }
   
@@ -36,6 +40,7 @@ class TrackDetailView:UIView {
     trackTitleLabel.text = viewModel.trackName
     authorTitleLabel.text = viewModel.artistName
     playTrack(preview: viewModel.previewUrl)
+    monitorStartTime()
     
     let string600 = viewModel.iconUrlString?.replacingOccurrences(of: "100x100", with: "600x600")
     guard let url = URL(string: string600 ?? "") else { return }
@@ -50,6 +55,31 @@ class TrackDetailView:UIView {
     player.replaceCurrentItem(with: playerItem)
     player.play()
   }
+  
+  //MARK: - Animations
+  
+  private func monitorStartTime() {
+    let time = CMTimeMake(value: 1, timescale: 3)
+    let times = [NSValue(time: time)]
+    player.addBoundaryTimeObserver(forTimes: times, queue: .main) { [weak self] in
+      self?.enlargeImageView()
+    }
+  }
+  
+  private func enlargeImageView() {
+    UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+      self.trackImageView.transform = .identity
+    }, completion: nil)
+  }
+  
+  private func reduceImageView() {
+    UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+      let scale: CGFloat = 0.8
+      self.trackImageView.transform = CGAffineTransform(scaleX: scale, y: scale)
+    }, completion: nil)
+  }
+  
+  //MARK: - @IBActions
   
   @IBAction func dragDownButtonTapped(_ sender: Any) {
     self.removeFromSuperview()
@@ -71,6 +101,7 @@ class TrackDetailView:UIView {
     if (player.timeControlStatus == .paused) {
       player.play()
       playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+      enlargeImageView()
     } else {
       player.pause()
       playPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
